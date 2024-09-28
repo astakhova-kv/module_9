3a5c021b720d60e24b70ee45a8a4f92b4a9a45a5
astakhova-kv <prisakarkv@gmail.com>
2024-09-28 22:15:55 +0700

    Update module_9_1.py

--- module_9_1.py
+++ module_9_1.py
@@ -1,8 +1,11 @@
 def apply_all_func(int_list, *functions):
     results = dict()
-    for func in functions:
-        results[func.__name__] = func(int_list)
-    return results
+    try:
+        for func in functions:
+            results[func.__name__] = func(int_list)
+        return results
+    except TypeError:
+        return 'Используйте список чисел'
 
-print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 'gh', 15, 9], max, min))
 print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
